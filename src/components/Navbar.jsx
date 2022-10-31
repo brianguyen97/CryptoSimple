9e0761c94975d0cb4376ea4fddec5e75c4e7f00f
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2022-10-31 12:28:29 -0600

    import navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      <Link to="/">
+        <h1>Cryptotrade</h1>
+      </Link>
+    </div>
+  );
 }
 
 export default Navbar;
