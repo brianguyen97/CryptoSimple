e0fa6587f22b553650c1a4bdd6413501d0eeb513
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2022-10-31 13:22:58 -0600

    HTML template for Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import ThemeToggle from "./ThemeToggle";
+import { AiOutlineMenu } from "react-icons/ai";
 
 function Navbar() {
   return (
@@ -7,6 +9,39 @@ function Navbar() {
       <Link to="/">
         <h1>Cryptotrade</h1>
       </Link>
+      <div>
+        <ThemeToggle />
+      </div>
+      <div>
+        <Link to="/signin">Sign In</Link>
+        <Link to="/signup">Sign Up</Link>
+      </div>
+      {/* Menu Icon */}
+      <div>
+        <AiOutlineMenu />
+      </div>
+      {/* Mobile Menu  */}
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/">Account</Link>
+          </li>
+          <li>
+            <ThemeToggle />
+          </li>
+        </ul>
+        <div>
+          <Link to="/signin">
+            <button>Sign In</button>
+          </Link>
+          <Link to="/signup">
+            <button>Sign Up</button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
