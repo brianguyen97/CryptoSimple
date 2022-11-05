6f0ddd0d9316bfc8e567538f3012ebbca3c44f9e
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2022-11-05 16:33:29 -0600

    Update CoinItem.jsx
    - HTML template for CoinItem
    - import AiOutlineStar, Sparklines, and uuid

--- src/components/CoinItem.jsx
+++ src/components/CoinItem.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import { AiOutlineStar } from "react-icons/ai";
+import { Sparklines, SparklinesLine } from "react-sparklines";
+import { v4 as uuidv4 } from "uuid";
 
-function CoinItem() {
-  return <div>CoinItem</div>;
+function CoinItem({ coin }) {
+  return (
+    <tr>
+      <td>
+        <AiOutlineStar />
+      </td>
+      <td>{coin.market_cap_rank}</td>
+      <td>
+        <div>
+          <img src={coin.image} alt={coin.id} />
+          <p>{coin.name}</p>
+        </div>
+      </td>
+      <td>{coin.symbol}</td>
+      <td>{coin.current_price}</td>
+      <td>{coin.price_change_percentage_24h}</td>
+      <td>{coin.total_volume}</td>
+      <td>{coin.market_cap}</td>
+      <td>
+        <Sparklines data={coin.sparkline_in_7d.price}>
+          <SparklinesLine color="teal" />
+        </Sparklines>
+      </td>
+    </tr>
+  );
 }
 
 export default CoinItem;
