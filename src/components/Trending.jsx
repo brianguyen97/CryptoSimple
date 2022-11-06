888160fe120537fc23061a25557a1906b3d1e4f4
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2022-11-06 16:00:01 -0700

    Update Trending.jsx
    - import useState, useEffect
    - import axios
    - useState to track trending array
    - save url to variable
    - useEffect call upon loading to set trending coins

--- src/components/Trending.jsx
+++ src/components/Trending.jsx
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function Trending() {
+  const [trending, setTrending] = useState([]);
+
+  const url = "https://api.coingecko.com/api/v3/search/trending";
+
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      setTrending(response.data.coins);
+      console.log(response.data.coins);
+    });
+  }, []);
+
   return <div>Trending</div>;
 }
 
