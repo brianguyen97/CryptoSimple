e9f0b58d5cf111da1b6e5fd8399ec5d8c0150d34
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2022-11-08 18:23:02 -0700

    styles for trending component

--- src/components/Trending.jsx
+++ src/components/Trending.jsx
@@ -13,20 +13,24 @@ function Trending() {
   }, []);
 
   return (
-    <div>
-      <h1>Trending Coins</h1>
-      <div>
+    <div className="rounded-div my-12 py-8 text-primary">
+      <h1 className="text-2xl font-bold py-4">Trending Coins</h1>
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4">
         {trending.map((coin) => (
-          <div>
-            <div>
-              <div>
-                <img src={coin.item.small} alt="/" />
+          <div className="rounded-div flex justify-between p-4 hover:scale-105 ease-in-out duration-300">
+            <div className="flex w-full items-center justify-between">
+              <div className="flex">
+                <img
+                  className="mr-4 rounded-full"
+                  src={coin.item.small}
+                  alt="/"
+                />
                 <div>
-                  <p>{coin.item.name}</p>
+                  <p className="font-bold">{coin.item.name}</p>
                   <p>{coin.item.symbol}</p>
                 </div>
               </div>
-              <div>
+              <div className="flex items-center">
                 <img
                   className="w-4 mr-2"
                   src="https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579"
