a2d856e65ee8988f0ab88b597de4dcf6ecd3a406
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2022-11-08 18:42:49 -0700

    axios api request for coinpage component

--- src/routes/CoinPage.jsx
+++ src/routes/CoinPage.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 function CoinPage() {
-  return <div>CoinPage</div>;
+  const [coin, setCoin] = useState({});
+
+  const url =
+    "https://api.coingecko.com/api/v3/coins/bitcoin?localization=false&sparkline=true";
+
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      setCoin(response.data);
+      console.log(response.data);
+    });
+  }, [url]);
+
+  return <div></div>;
 }
 
 export default CoinPage;
