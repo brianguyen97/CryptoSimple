b37d8065a3157e9f362d6e09def19042e1e04265
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2022-11-23 23:17:51 -0700

    display data from api for coin

--- src/routes/CoinPage.jsx
+++ src/routes/CoinPage.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { Sparklines, SparklinesLine } from "react-sparklines";
 
 function CoinPage() {
   const [coin, setCoin] = useState({});
@@ -14,7 +15,132 @@ function CoinPage() {
     });
   }, [url]);
 
-  return <div></div>;
+  return (
+    <div>
+      <div>
+        <img src={coin.image?.large} alt="/" />
+        <div>
+          <p>{coin?.name} Price</p>
+          <p>({coin.symbol?.toUpperCase()} / USD)</p>
+        </div>
+      </div>
+
+      <div>
+        <div>
+          <div>
+            {coin.market_data?.current_price ? (
+              <p>${coin.market_data.current_price.usd.toLocaleString()}</p>
+            ) : null}
+            <p>7 Day</p>
+          </div>
+          <div>
+            <Sparklines data={coin.market_data?.sparkline_7d.price}>
+              <SparklinesLine color="teal" />
+            </Sparklines>
+          </div>
+          <div>
+            <div>
+              <p>Market Cap</p>
+              {coin.market_data?.market_cap ? (
+                <p>${coin.market_data.market_cap.usd.toLocaleString()}</p>
+              ) : null}
+            </div>
+            <div>
+              <p>Volume (24h)</p>
+              {coin.market_data?.market_cap ? (
+                <p>${coin.market_data.total_volume.usd.toLocaleString()}</p>
+              ) : null}
+            </div>
+          </div>
+          <div>
+            <div>
+              <p>24h High</p>
+              {coin.market_data?.high_24h ? (
+                <p>${coin.market_data.high_24h.usd.toLocaleString()}</p>
+              ) : null}
+            </div>
+            <div>
+              <p>24h Low</p>
+              {coin.market_data?.low_24h ? (
+                <p>${coin.market_data.low_24h.usd.toLocaleString()}</p>
+              ) : null}
+            </div>
+          </div>
+        </div>
+        <div>
+          <p>Market Stats</p>
+          <div>
+            <div>
+              <p>Market Rank</p>
+              {coin.markket_cap_rank}
+            </div>
+            <div>
+              <p>Hashing Algorithm</p>
+              {coin.hashing_algorithm ? <p>{coin.hashing_algorithm}</p> : null}
+            </div>
+            <div>
+              <p>Trust Score</p>
+              {coin.tickers ? <p>{coin.liquidity_score.toFixed(2)}</p> : null}
+            </div>
+
+            <div>
+              <div>
+                <p>Price Change (24h)</p>
+                {coin.market_data ? (
+                  <p>
+                    {coin.market_data.price_change_percentage_24h.toFixed(2)}%
+                  </p>
+                ) : null}
+              </div>
+              <div>
+                <p>Price Change (7d)</p>
+                {coin.market_data ? (
+                  <p>
+                    {coin.market_data.price_change_percentage_7d.toFixed(2)}%
+                  </p>
+                ) : null}
+              </div>
+              <div>
+                <p>Price Change (14d)</p>
+                {coin.market_data ? (
+                  <p>
+                    {coin.market_data.price_change_percentage_14d.toFixed(2)}%
+                  </p>
+                ) : null}
+              </div>
+            </div>
+
+            <div>
+              <div>
+                <p>Price Change (30d)</p>
+                {coin.market_data ? (
+                  <p>
+                    {coin.market_data.price_change_percentage_30d.toFixed(2)}%
+                  </p>
+                ) : null}
+              </div>
+              <div>
+                <p>Price Change (60d)</p>
+                {coin.market_data ? (
+                  <p>
+                    {coin.market_data.price_change_percentage_60d.toFixed(2)}%
+                  </p>
+                ) : null}
+              </div>
+              <div>
+                <p>Price Change (1y)</p>
+                {coin.market_data ? (
+                  <p>
+                    {coin.market_data.price_change_percentage_1y.toFixed(2)}%
+                  </p>
+                ) : null}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default CoinPage;
