97231c24883eedf335d6bdc265eb1ea485d7faa7
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2023-01-03 12:24:34 -0700

    Markup for Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,55 @@
 import React from "react";
+import { AiOutlineInstagram } from "react-icons/ai";
+import { FaFacebookF, FaGithub, FaTiktok, FaTwitter } from "react-icons/fa";
+import ThemeToggle from "./ThemeToggle";
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div>
+      <div>
+        <div>
+          <h2>Support</h2>
+          <ul>
+            <li>Help Centre</li>
+            <li>Contact Us</li>
+            <li>API Status</li>
+            <li>Documentation</li>
+          </ul>
+        </div>
+        <div>
+          <h2>Info</h2>
+          <ul>
+            <li>About Us</li>
+            <li>Careers</li>
+            <li>Invest</li>
+            <li>Legal</li>
+          </ul>
+        </div>
+      </div>
+      <div>
+        <div>
+          <div>
+            <ThemeToggle />
+          </div>
+          <p>Sign Up for Crypto News</p>
+          <div>
+            <form>
+              <input type="email" placeholder="E-Mail" />
+              <button>Sign Up</button>
+            </form>
+          </div>
+          <div>
+            <AiOutlineInstagram />
+            <FaTiktok />
+            <FaTwitter />
+            <FaFacebookF />
+            <FaGithub />
+          </div>
+        </div>
+      </div>
+      <p className="text-center py-4">Powered by Coin Gecko</p>
+    </div>
+  );
 }
 
 export default Footer;
