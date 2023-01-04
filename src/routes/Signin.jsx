07613ff283ffe8c23cb27ec0af44c912e2f26079
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2023-01-04 11:01:26 -0700

    Tailwind styles for Sign In page

--- src/routes/Signin.jsx
+++ src/routes/Signin.jsx
@@ -5,28 +5,38 @@ import { Link } from "react-router-dom";
 function Signin() {
   return (
     <div>
-      <div>
-        <h1>Sign In</h1>
+      <div className="max-w-[400px] mx-auto min-h-[600px] px-4 py-20">
+        <h1 className="text-2xl font-bold">Sign In</h1>
         <form>
-          <div>
+          <div className="my-4">
             <label>Email</label>
-            <div>
-              <input type="email" />
-              <AiOutlineMail />
+            <div className="my-2 w-full relative rounded-2xl shadow-xl">
+              <input
+                className="w-full p-2 bg-primary border border-input rounded-2xl"
+                type="email"
+              />
+              <AiOutlineMail className="absolute right-2 top-3 text-gray-400" />
             </div>
           </div>
-          <div>
+          <div className="my-4">
             <label>Password</label>
-            <div>
-              <input type="password" />
-              <AiFillLock />
+            <div className="my-2 w-full relative rounded-2xl shadow-xl">
+              <input
+                className="w-full p-2 bg-primary border border-input rounded-2xl"
+                type="password"
+              />
+              <AiFillLock className="absolute right-2 top-3 text-gray-400" />
             </div>
           </div>
-          <button>Sign In</button>
+          <button className="w-full my-2 p-3 bg-button text-btnText rounded-2xl shadow-xl">
+            Sign In
+          </button>
         </form>
-        <p>
+        <p className="my-4">
           Don't have an account?{" "}
-          <Link to="/signup">Click Here to Sign Up.</Link>
+          <Link className="text-accent" to="/signup">
+            Sign Up.
+          </Link>
         </p>
       </div>
     </div>
