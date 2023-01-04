d2ac89db502032300750a2685b6556cd0bc0ad5c
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2023-01-04 10:44:57 -0700

    Markup for Sign In page

--- src/routes/Signin.jsx
+++ src/routes/Signin.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import { AiFillLock, AiOutlineMail } from "react-icons/ai";
+import { Link } from "react-router-dom";
 
 function Signin() {
-  return <div>Signin</div>;
+  return (
+    <div>
+      <div>
+        <h1>Sign In</h1>
+        <form>
+          <div>
+            <label>Email</label>
+            <div>
+              <input type="email" />
+              <AiOutlineMail />
+            </div>
+          </div>
+          <div>
+            <label>Password</label>
+            <div>
+              <input type="password" />
+              <AiFillLock />
+            </div>
+          </div>
+          <button>Sign In</button>
+        </form>
+        <p>
+          Don't have an account?{" "}
+          <Link to="/signup">Click Here to Sign Up.</Link>
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Signin;
