266b1a2e7fbb1317280fe754177b202b20f01377
Brian Nguyen <97560651+brianguyen97@users.noreply.github.com>
2023-01-05 22:23:22 -0700

    Markup for account page, import SavedCoins component

--- src/routes/Account.jsx
+++ src/routes/Account.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import SavedCoins from "./SavedCoins";
 
 function Account() {
-  return <div>Account</div>;
+  return (
+    <div>
+      <div>
+        <div>
+          <h1>Account</h1>
+          <div>
+            <p>Welcome, User</p>
+          </div>
+        </div>
+        <div>
+          <button>Sign Out</button>
+        </div>
+      </div>
+      <div>
+        <div>
+          <h1>Watch List</h1>
+          <SavedCoins />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Account;
